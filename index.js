0b5e7e95d88cae0334f6a5cd7f10825f99ee9e0b
Jonathan Taylor <jhhtaylor@gmail.com>
2021-04-24 19:55:20 +0200

    Added Lab 2 Stuff

---
+++ index.js
@@ -0,0 +1,12 @@
+//import { mainRouter as MR } from "mainRoutes.js";
+
+let path = require("path");
+let express = require("express");
+let app = express();
+let mainRouter = require("./mainRoutes.js");
+
+app.use(mainRouter);
+let port = process.env.PORT || 3000;
+app.listen(port);
+console.log("Express server running on port", port);
+console.log("Express server running on port 3000");
